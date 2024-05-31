aa2ddc028524c92c38c7b177e5be1a355cf98523
everett-ripley <everett.ripley@gmail.com>
2024-05-31 00:47:05 -0400

    Day 1
    Basic state machine for colour node
    
    Colour enum
    
    Shader colour variable

--- Scenes/colour_node.gd
+++ Scenes/colour_node.gd
@@ -1,10 +1,27 @@
 extends Node2D
 
+@export_enum("red",
+ "red_orange",
+ "orange",
+ "yellow_orange",
+ "yellow",
+ "yellow_green",
+ "green",
+"blue_green",
+"blue",
+"blue_violet",
+"violet") var colour = 0
+
+var colour_values : Array[Vector3] = [
+	
+]
+
 enum states {
 	inert,
 	active,
 	dead
 }
+var state = states.inert
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,3 +31,23 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_mouse_collider_mouse_entered():
+	match state:
+		states.inert:
+			pass
+		states.active:
+			pass
+		states.dead:
+			return
+
+
+func _on_mouse_collider_mouse_exited():
+	match state:
+		states.inert:
+			pass
+		states.active:
+			pass
+		states.dead:
+			return
