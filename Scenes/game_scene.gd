424ce33bc002a127090195079aa5a8aa08d70c65
everett-ripley <everett.ripley@gmail.com>
2024-06-01 15:16:50 -0400

    Working on better generation
    This version is buggy as hell. But as bad and laggy as it is, it actually looks pretty cool, hence why I'm saving it here.

--- Scenes/game_scene.gd
+++ Scenes/game_scene.gd
@@ -1,12 +1,15 @@
 extends Node2D
 @onready var chunk = preload("res://Scenes/chunk.tscn")
 @onready var camera := $Camera2D
+@onready var mouse := $Mouse
 @export var chunk_unit_size : float = 60.0
 @export var chunk_x_units : int = 10
 @export var chunk_y_units : int = 10
 @export var initial_size : int = 3 #describes both rows and columns
 @export var camera_limit : float = 500.0
 
+const generate_distance : int = 2
+var generated_chunks = {}
 var chunk_height : float
 var chunk_width : float
 
@@ -21,7 +24,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	generate_new_chunks_from_position(mouse.global_position/chunk_unit_size)
 
 func update_camera_limit(amount:float):
 	camera_limit += amount
@@ -37,10 +40,34 @@ func generate_map():
 		for x in initial_size:
 			var pos : Vector2 = Vector2(x * chunk_width,y * chunk_height) + offset
 			add_chunk(pos)
-	
+			register_chunk(x,y)
+	print(generated_chunks)
 
-func expand_edges():
+
+func check_for_chunk(x,y):
+	if x in generated_chunks and y in generated_chunks[x] and generated_chunks[x][y] == true:
+		return true
+	else:
+		return false
+
+func register_chunk(x,y):
+	if x in generated_chunks:
+		generated_chunks[x][y] = true
+	else:
+		generated_chunks[x] = {y : true}
+
+func generate_new_chunk(x,y):
+	if !check_for_chunk(x,y):
+		add_chunk(Vector2(x*chunk_unit_size,y*chunk_unit_size))
+		register_chunk(x,y)
 	pass
+
+func generate_new_chunks_from_position(pos:Vector2):
+	for x in range(generate_distance*2):
+		x += pos.x - generate_distance
+		for y in range(generate_distance*2):
+			y += pos.y - generate_distance
+			generate_new_chunk(x,y)
 
 
 func add_chunk(pos:Vector2):
