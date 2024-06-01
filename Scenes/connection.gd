b25398553135a342f8c85deec9708426767030ee
everett-ripley <everett.ripley@gmail.com>
2024-05-31 23:36:54 -0400

    Gradient mix function added to connections
    Note:
    This doesn't work possibly due to an issue with Godot.
    For some reason the middle sections of the gradient aren't being displayed.
    If this persists, use the mixed colour on the starting point of the connection.

--- Scenes/connection.gd
+++ Scenes/connection.gd
@@ -13,7 +13,8 @@ var mouse:Area2D
 func _ready():
 	line.gradient = gradient.duplicate()
 	update_gradient(Vector3(1.0,1.0,1.0), 1)
-	update_gradient(Vector3(1.0,1.0,1.0), 1)
+	update_gradient(Vector3(1.0,1.0,1.0), 2)
+	update_gradient(Vector3(1.0,1.0,1.0), 3)
 
 
 func _process(delta):
@@ -46,22 +47,62 @@ func interpolate_position(vec:Vector2):
 func update_gradient(color:Vector3, point:int):
 	line.gradient.set_color(point, Color(color[0],color[1],color[2]))
 
+#unideal solution, since it's almost identical logic to the colour_node. Ideally this would be moved to some kind of library
+func mix_nodes(parent_colour:int, target_colour:int)->Vector3:
+	var colour_values : Array[Vector3] = [
+	Vector3(1.0,0.0,0.0), #red
+	Vector3(1.0, 0.3, 0.0), #red_orange
+	Vector3(1.0, 0.5, 0.0), #orange
+	Vector3(1.0, 0.8, 0.0), #yellow_orange
+	Vector3(1.0,1.0,0.0), #yellow
+	Vector3(0.75,1.0,0.0), #yellow_green
+	Vector3(0.0,1.0,0.0), #green
+	Vector3(0.0,0.5,0.5), #blue_green
+	Vector3(0.0,0.0,1.0), #blue
+	Vector3(0.33,0.0,1.0), #blue_violet
+	Vector3(0.6,0.0,0.6), #violet
+	Vector3(0.8, 0.0, 0.4) #red_violet
+	]
+	var difference = abs(target_colour - parent_colour)
+	if difference == 6 or difference == 0:
+		return Vector3(1.0,1.0,1.0)
+	var return_colour : int
+	if difference == 1:
+		return_colour = target_colour
+	elif difference < 6:
+		return_colour = (parent_colour + target_colour)/2
+	elif difference > 6:
+		return_colour = (parent_colour + target_colour)/2 + 6
+	if return_colour < 0:
+		return_colour += 11
+	elif return_colour > 11:
+		return_colour -= 11
+	return colour_values[return_colour]
+
+
 func parent_updated(col:Vector3):
 	update_gradient(col, 0)
+	update_gradient(mix_nodes(parent_node.colour, target_node.colour), 1)
+	update_gradient(mix_nodes(parent_node.colour, target_node.colour), 2)
 
 func target_updated(col:Vector3):
-	update_gradient(col, 2)
+	update_gradient(col, 3)
+	update_gradient(mix_nodes(parent_node.colour, target_node.colour), 1)
+	update_gradient(mix_nodes(parent_node.colour, target_node.colour), 2)
 
 func _on_detector_area_entered(area):
 	if area.is_in_group("ColourNodeArea"):
 		target_node = area.get_parent()
 		var tween = create_tween()
-		tween.tween_method(update_gradient.bind(2), Vector3(line.gradient.get_color(2).r, line.gradient.get_color(2).g, line.gradient.get_color(2).b), target_node.colour_values[target_node.colour], 0.3)
-		#update_gradient(2, target_node.colour_values[target_node.colour])
+		tween.tween_method(update_gradient.bind(3), Vector3(line.gradient.get_color(3).r, line.gradient.get_color(3).g, line.gradient.get_color(3).b), target_node.colour_values[target_node.colour], 0.3)
+		tween.parallel().tween_method(update_gradient.bind(1), Vector3(line.gradient.get_color(1).r, line.gradient.get_color(1).g, line.gradient.get_color(1).b), mix_nodes(parent_node.colour, target_node.colour), 0.3)
+		tween.parallel().tween_method(update_gradient.bind(2), Vector3(line.gradient.get_color(2).r, line.gradient.get_color(2).g, line.gradient.get_color(2).b), mix_nodes(parent_node.colour, target_node.colour), 0.3)
 
 
 func _on_detector_area_exited(area):
 	if area.is_in_group("ColourNodeArea"):
 		target_node = null
 		var tween = create_tween()
-		tween.tween_method(update_gradient.bind(2), Vector3(line.gradient.get_color(2).r, line.gradient.get_color(2).g, line.gradient.get_color(2).b), Vector3(1.0,1.0,1.0), 0.3)
+		tween.tween_method(update_gradient.bind(3), Vector3(line.gradient.get_color(3).r, line.gradient.get_color(3).g, line.gradient.get_color(3).b), Vector3(1.0,1.0,1.0), 0.3)
+		tween.parallel().tween_method(update_gradient.bind(1), Vector3(line.gradient.get_color(1).r, line.gradient.get_color(1).g, line.gradient.get_color(1).b), Vector3(1.0,1.0,1.0), 0.3)
+		tween.parallel().tween_method(update_gradient.bind(2), Vector3(line.gradient.get_color(2).r, line.gradient.get_color(2).g, line.gradient.get_color(2).b), Vector3(1.0,1.0,1.0), 0.3)
