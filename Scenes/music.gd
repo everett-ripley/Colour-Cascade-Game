9f608e3f02da9a75b2c9ec312dcc9b6bb3f2870d
everett-ripley <everett.ripley@gmail.com>
2024-06-02 13:09:48 -0400

    bug fixes

--- Scenes/music.gd
+++ Scenes/music.gd
@@ -3,7 +3,11 @@ extends Node
 @onready var loop := $Loop
 
 func _ready():
+	loop.volume_db = linear_to_db(0.0)
+	loop.play()
+	loop.stop()
 	intro.play()
+	loop.volume_db = 0.0
 
 func _on_intro_finished():
 	loop.play()
